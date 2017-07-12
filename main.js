fefa1a6ca08883f786ff2f1b2c0fd2b46d6555de
mohd tayyab <mohdrz789@gmail.com>
2017-07-12 14:50:32 +0530

    bugs resolved

--- main.js
+++ main.js
@@ -40,35 +40,72 @@ $('.fa-random').on('click',function() {
 
 
 
-    $('audio').on('ended',function() {
-      var audio = document.querySelector('audio');
-      if (willShuffle == 1) {
-          var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
-          var nextSongObj = songs[nextSongNumber-1];
-          audio.src = nextSongObj.fileName;
-          toggleSong();
-          changeCurrentSongDetails(nextSongObj);
-          currentSongNumber = nextSongNumber;
-      }
-      else if(currentSongNumber < 4) {
-          var nextSongObj = songs[currentSongNumber];
-          audio.src = nextSongObj.fileName;
-          toggleSong();
-          changeCurrentSongDetails(nextSongObj);
-          currentSongNumber = currentSongNumber + 1;
-      }
-      else if(willLoop == 1) {
-          var nextSongObj = songs[0];
-          audio.src = nextSongObj.fileName;
-          toggleSong();
-          changeCurrentSongDetails(nextSongObj);
-          currentSongNumber =  1;
-      }
-      else {
-          $('.play-icon').removeClass('fa-pause').addClass('fa-play');
-          audio.currentTime = 0;
-      }
-  })
+    //$('audio').on('ended',function() {
+    //  var audio = document.querySelector('audio');
+    //  if (willShuffle == 1) {
+      //    var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
+      //    var nextSongObj = songs[nextSongNumber-1];
+      //    audio.src = nextSongObj.fileName;
+      //    toggleSong();
+      //    changeCurrentSongDetails(nextSongObj);
+      //    currentSongNumber = nextSongNumber;
+    //  }
+    //  else if(currentSongNumber < 4) {
+      //    var nextSongObj = songs[currentSongNumber];
+      //    audio.src = nextSongObj.fileName;
+      //    toggleSong();
+        //  changeCurrentSongDetails(nextSongObj);
+        //  currentSongNumber = currentSongNumber + 1;
+    //  }
+    //  else if(willLoop == 1) {
+      //    var nextSongObj = songs[0];
+        //  audio.src = nextSongObj.fileName;
+        //  toggleSong();
+        //  changeCurrentSongDetails(nextSongObj);
+        //  currentSongNumber =  1;
+    //  }
+    //  else {
+      //    $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+      //    audio.currentTime = 0;
+    //  }
+//  })
+
+//The actual looping code. Runs only if looping is enabled.
+$('audio').on('ended', function() {
+  // Run the following code only if willLoop == 1, i.e., if the player will loop.
+  if(willLoop) {
+    var audio = document.querySelector('audio');
+    if(currentSongNumber < songs.length) {
+      // PLay the next song
+      var nextSongObj = songs[currentSongNumber];
+      audio.src = nextSongObj.fileName;   // Change the source
+      toggleSong();   // Play the next song
+      changeCurrentSongDetails(nextSongObj);  // Change the song details in the player controls
+      currentSongNumber++;  // Increment the current song number.
+    }
+    else {
+      // Play the first song
+      audio.src = songs[0].fileName;
+      toggleSong();
+      changeCurrentSongDetails(songs[0]);
+      currentSongNumber = 1;
+    }
+  }
+});
+
+$('audio').on('ended', function() {
+  if(willShuffle) {
+    audio = document.querySelector('audio');
+    currentSongNumber = Math.floor((Math.random() * 4) + 1);
+    var nextSongObj = songs[currentSongNumber - 1];
+    audio.src = nextSongObj.fileName;
+    toggleSong();
+    changeCurrentSongDetails(nextSongObj);
+  }
+});
+
+
+
 
 function toggleSong() {
 
@@ -263,74 +300,133 @@ $('.play-icon').on('click', function() {//on click button function
 
 
           var songNumber=1;
-          function addSongNameClickEvent(songObj,position) {
+        //  function addSongNameClickEvent(songObj,position) {
 
 
 
-                var songName = songObj.fileName; // New Variable
+          //      var songName = songObj.fileName; // New Variable
         //  function addSongNameClickEvent(songName,position) {
-            var id = '#song' + position; //if song position is 1 then song is"song1"
-          $(id).click(function() {//func for song 1
-              Playingnumber=position-1;
-            var audio = document.querySelector('audio');
-            var currentSong = audio.src;
-            if(songNumber !=position)  //If the string is not found, it gives us the -1 value else location of string
+          //  var id = '#song' + position; //if song position is 1 then song is"song1"
+        //  $(id).click(function() {
 
-            {audio.src =songName;
-              songNumber=position;
+
+            //func for song 1
+          //    Playingnumber=position-1;
+          //  var audio = document.querySelector('audio');
+          //  var currentSong = audio.src;
+          //  if(songNumber !=position)  //If the string is not found, it gives us the -1 value else location of string
+
+          //  {audio.src =songName;
+          //    songNumber=position;
                 //toggleSong();
-                changeCurrentSongDetails(songObj); // Function Call
-              }
-            //  else {
-                toggleSong();
+            //    changeCurrentSongDetails(songObj); // Function Call
             //  }
-            });
+            //  else {
+            //    toggleSong();
+            //  }
+
+          //  });
 
 
 
-        }
+        //}
 
-          $(".fa-step-forward").click(function(){
-            if(Playingnumber==songs.length-1){
-                console.log("1");
-              Playingnumber=0;
-              changesong();
-
-            }
-
-            else {
-              console.log("2");
-              console.log("Playingnumber")
-              Playingnumber++;
-              changesong();
-            }
-          })
-
-          $(".fa-step-backward").click(function(){
-            if(Playingnumber==0){
-                console.log("1");
-                Playingnumber=songs.length-1;
-                changesong();
-              }
-              else {
-                  Playingnumber--;
-                  changesong();
-              }
-            })
+        function addSongNameClickEvent(song, position) {
+  var songId = '#song' + position;
+  var fileName = song.fileName;
+  $(songId).on('click', function() {
+    currentSongNumber = (position > 0 && position <= songs.length) ? position : ((position > songs.length) ? songs.length : 1);
+    var audio = document.querySelector('audio');
+    if(audio.src.search(fileName) != -1) {
+      toggleSong();
+    }
+    else {
+      audio.src = fileName;
+      toggleSong();
+      changeCurrentSongDetails(song);
+    }
+  });
+}
 
 
 
 
 
-          function changesong()
-          {
-            var music=songs[Playingnumber].fileName;
-            var song=document.querySelector("audio");
-            song.src=music;
+        //  $(".fa-step-forward").click(function(){
+          //  if(Playingnumber==songs.length-1){
+            //    console.log("1");
+            //  Playingnumber=0;
+          //    changesong();
+          //  }
+          //  else {
+            //  console.log("2");
+            //  console.log("Playingnumber")
+            //  Playingnumber++;
+            // changesong();
+          //  }
+        //  })
+
+        $('.fa-step-forward').on('click', function() {
+          var audio = document.querySelector('audio');
+          if(currentSongNumber < songs.length) {
+            currentSongNumber++;
+            var nextSongObj = songs[currentSongNumber - 1];
+            audio.src = nextSongObj.fileName;
             toggleSong();
-            changeCurrentSongDetails(songs[Playingnumber])
-            updateTimer();
+            changeCurrentSongDetails(nextSongObj);
           }
+          else {
+            currentSongNumber = 1;
+            audio.src = songs[0].fileName;
+            toggleSong();
+            changeCurrentSongDetails(songs[0]);
+          }
+        });
+
+
+
+
+      //    $(".fa-step-backward").click(function(){
+          //  if(Playingnumber==0){
+          //      console.log("1");
+          //      Playingnumber=songs.length-1;
+          //      changesong();
+          //    }
+          //    else {
+          //        Playingnumber--;
+            //      changesong();
+            ////  }
+          //  })
+
+
+
+
+
+        //  function changesong()
+          //{
+          //  var music=songs[Playingnumber].fileName;
+          //  var song=document.querySelector("audio");
+          //  song.src=music;
+          //  toggleSong();
+          //  changeCurrentSongDetails(songs[Playingnumber])
+          //  updateTimer();
+        //  }
+        $('.fa-step-backward').on('click', function() {
+  var audio = document.querySelector('audio');
+  if(currentSongNumber >= 1) {
+    currentSongNumber--;
+    var prevSongObj = songs[currentSongNumber - 1];
+    audio.src = prevSongObj.fileName;
+    toggleSong();
+    changeCurrentSongDetails(prevSongObj);
+  }
+  else {
+    currentSongNumber = songs.length;
+    audio.src = songs[currentSongNumber - 1].fileName;
+    toggleSong();
+    changeCurrentSongDetails(songs[currentSongNumber - 1]);
+  }
+});
 
           function updateTimer()
             {
@@ -420,14 +516,30 @@ $('.play-icon').on('click', function() {//on click button function
         console.log(changemenu);
        if (changemenu == 1)
   {
-        $('.wrapper').removeClass('hidden');
+        $('.mymenu').removeClass('hidden');
+          $('.drumplaylist').removeClass('hidden');
+          $('.fa-repeat').addClass('disabled');
+            $('.fa-random').addClass('disabled');
+            $('.fa-step-backward').addClass('disabled');
+            $('.fa-step-forward').addClass('disabled');
+           $('.fa-caret-square-o-right').addClass('disabled');
+            $('.fa-caret-square-o-left').addClass('disabled');
+            $('.fa-play').addClass('disabled');
+            $('.fa-pause').addClass('disabled');
             $('.content').addClass('hidden');
 
       }
         else
         {
           $('.content').removeClass('hidden');
-          $('.wrapper').addClass('hidden');
+          $('.fa-step-backward').removeClass('disabled');
+          $('.fa-step-forward').removeClass('disabled');
+          $('.fa-caret-square-o-right').removeClass('disabled');
+           $('.fa-caret-square-o-left').removeClass('disabled');
+           $('.fa-play').removeClass('disabled');
+           $('.fa-pause').removeClass('disabled');
+             $('.drumplaylist').addClass('hidden');
+          $('.mymenu').addClass('hidden');
         }
      });
 
